3dc56b3574d78c9354037052a0ec6c72afe793ae
ssstroud23 <ssstroud23@gmail.com>
2018-07-09 19:25:36 +0000

    Done.

---
+++ stock_supermarket.sh
@@ -0,0 +1,27 @@
+mv kwk-students-11-det-070918/delivery/apples.txt kwk-students-11-det-070918/fruit;
+mv kwk-students-11-det-070918/delivery/bison_burgers.txt kwk-students-11-det-070918/meat;
+mv kwk-students-11-det-070918/delivery/brie.txt kwk-students-11-det-070918/dairy
+mv kwk-students-11-det-070918/delivery/broccoli.txt kwk-students-11-det-070918 kwk-students-11-det-070918/veggies
+mv kwk-students-11-det-070918/delivery/carrot_cake.txt kwk-students-11-det-070918/dessert
+mv kwk-students-11-det-070918/delivery/carrots.txt kwk-students-11-det-070918/veggies
+mv kwk-students-11-det-070918/delivery/eggo_waffles.txt kwk-students-11-det-070918/freezer
+mv kwk-students-11-det-070918/delivery/fage_yogurt.txt kwk-students-11-det-070918/dairy
+mv kwk-students-11-det-070918/delivery/fig_bars.txt kwk-students-11-det-070918/snax
+mv kwk-students-11-det-070918/delivery/flourless_cake.txt kwk-students-11-det-070918/dessert
+mv kwk-students-11-det-070918/delivery/garlic.txt kwk-students-11-det-070918/veggies
+mv kwk-students-11-det-070918/delivery/goldfish.txt kwk-students-11-det-070918/snax
+mv kwk-students-11-det-070918/delivery/grapples.txt kwk-students-11-det-070918/fruit
+mv kwk-students-11-det-070918/delivery/hotdogs.txt kwk-students-11-det-070918/meat
+mv kwk-students-11-det-070918/delivery/icecream.txt kwk-students-11-det-070918/dairy
+mv kwk-students-11-det-070918/delivery/lamb_ribs.txt kwk-students-11-det-070918/meat
+mv kwk-students-11-det-070918/delivery/land_o_lakes_butter.txt kwk-students-11-det-070918/dairy
+mv kwk-students-11-det-070918/delivery/lean_cuisine.txt kwk-students-11-det-070918/meat
+mv kwk-students-11-det-070918/delivery/leeks.txt kwk-students-11-det-070918/veggies
+mv kwk-students-11-det-070918/delivery/milk.txt kwk-students-11-det-070918/dairy
+mv kwk-students-11-det-070918/delivery/mint_milano_cookies.txt kwk-students-11-det-070918/dessert
+mv kwk-students-11-det-070918/delivery/onion.txt kwk-students-11-det-070918/veggies
+kwk-students-11-det-070918/delivery/oreos.txt kwk-students-11-det-070918/dessert
+kwk-students-11-det-070918/delivery/potatoes.txt kwk-students-11-det-070918/veggies
+kwk-students-11-det-070918/delivery/salmon_steak.txt kwk-students-11-det-070918/meat
+kwk-students-11-det-070918/delivery/tiramisu_cake.txt kwk-students-11-det-070918/dessert
+kwk-students-11-det-070918/delivery/tomatoes.txt kwk-students-11-det-070918/veggies
